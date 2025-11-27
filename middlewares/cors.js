05944a1977817fd410e7a5c10a20ba0bb5da32f2
Matías Rojas <matiasigrojasr.inginfo@gmail.com>
2025-11-27 08:18:11 -0300

    Add new origin to CORS accepted origins

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -4,7 +4,8 @@ const ACCEPTED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:3000',
     'https://movies.com',
-    'https://midu.dev'
+    'https://midu.dev',
+    'calm-respect-deploy-api-with-mysql.up.railway.app'
 ]
 
 export const corsMiddleware = ({acceptedOrigins = ACCEPTED_ORIGINS} = {}) => cors({
